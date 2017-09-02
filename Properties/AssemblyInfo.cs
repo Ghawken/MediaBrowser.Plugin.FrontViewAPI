3fc0f8cd6edb9f85eeb3ad60ea2bbd1243a40f35
Glenn Hawken <ghawken@hotkey.net.au>
2017-09-02 12:18:17 +1000

    1.0.0.5 Server Core updates

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyVersion("1.0.0.50")]
+[assembly: AssemblyVersion("1.0.0.55")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
